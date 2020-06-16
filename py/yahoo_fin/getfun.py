c46680daef42121b12d8261a00fe628fc087d5d2
Michael Angerman <stormasm@gmail.com>
2020-06-16 16:14:00 -0700

    add in the get_symbols method

--- py/yahoo_fin/getfun.py
+++ py/yahoo_fin/getfun.py
@@ -1,10 +1,14 @@
 import datetime
 from yahoo_fin.stock_info import get_stats
 
-# This is a Python set which is nice because we can not
-# get any duplication of data...
-symbols = {"ui","psa","ip","t"}
-symbols.add("nly")
+# This should return a set of symbols
+def get_symbols(filename):
+    myset = set()
+    fp = open(filename, "r")
+    for cnt, line in enumerate(fp):
+        symbol = line.strip()
+        myset.add(symbol)
+    return(myset)
 
 def get_day():
     x = datetime.datetime.now()
@@ -16,11 +20,19 @@ def build_file_name(symbol):
     filename = f"{symbol}-fun-{day}.csv"
     return(filename)
 
-def process():
+def process(symbols):
     for symbol in symbols:
         print(symbol)
         filename = build_file_name(symbol)
         data = get_stats(symbol)
         data.to_csv(filename)
 
-process()
+if __name__ == "__main__":
+    path = '/j/tmp32/equity-data/symbols/top.txt'
+    symbols = get_symbols(path)
+    process(symbols)
+
+# This is a Python set which is nice because we can not
+# get any duplication of data...
+# symbols = {"ui","psa","ip","t"}
+# symbols.add("nly")
