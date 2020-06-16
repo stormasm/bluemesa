6ad993d4e30b29b72b284e55e5bc4ef7b358ab99
Michael Angerman <stormasm@gmail.com>
2020-06-16 09:47:59 -0700

    mw

--- py/redis/funlist.py
+++ py/redis/funlist.py
@@ -4,7 +4,7 @@ import redis
 rc = redis.Redis(host='localhost', port=6379, db=0)
 
 with open('/j/tmp32/bluemesa/tmp/ui-fun-20-06-16.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-    for row in spamreader:
+    funreader = csv.reader(csvfile, delimiter=',')
+    for row in funreader:
         #print(', '.join(row))
         print(row[0],row[2])
