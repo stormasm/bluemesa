72590e50a2d1419bf982f9a2ef809d6864a95c02
Michael Angerman <stormasm@gmail.com>
2020-06-16 11:02:36 -0700

    no longer reading the first line when using csv reader

--- py/redis/funlist.py
+++ py/redis/funlist.py
@@ -1,10 +1,26 @@
 import csv
+
+from os import listdir
+from os.path import isfile, join
+
 import redis
 
 rc = redis.Redis(host='localhost', port=6379, db=0)
 
-with open('/j/tmp32/bluemesa/tmp/ui-fun-20-06-16.csv', newline='') as csvfile:
-    funreader = csv.reader(csvfile, delimiter=',')
-    for row in funreader:
-        #print(', '.join(row))
-        print(row[0],row[2])
+def getfiles(mypath):
+    onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+    return(onlyfiles)
+
+def write_file_to_redis(filename):
+    with open(filename, newline='') as csvfile:
+        funreader = csv.reader(csvfile, delimiter=',')
+        next(funreader)
+        for row in funreader:
+            print(row[0],row[2])
+
+if __name__ == "__main__":
+    path = '/j/tmp32/bluemesa/tmp'
+    myfiles = getfiles(path)
+    for filename in myfiles:
+        file = path + '/' + filename
+        write_file_to_redis(file)
