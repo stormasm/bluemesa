89f81d91044fb9b31259e37c0f6f483dd8a6fb47
Michael Angerman <stormasm@gmail.com>
2020-06-16 09:23:36 -0700

    starting to work on readfun csv reader and write the data to redis

--- py/redis/funlist.py
+++ py/redis/funlist.py
@@ -1,8 +1,2 @@
 import redis
-r = redis.Redis(host='localhost', port=6379, db=0)
-r.set('foo', 'bar')
-x = r.get('foo')
-print(x)
-r.set(x,'baz')
-x = r.get(x)
-print(x)
+rc = redis.Redis(host='localhost', port=6379, db=0)
