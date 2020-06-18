2ef54a21c83fee3df21ce37a43454dfbf2fe474d
Michael Angerman <stormasm@gmail.com>
2020-06-18 16:08:33 -0700

    mw

--- py/sp500/util.py
+++ py/sp500/util.py
@@ -5,6 +5,15 @@ def get_symbol_dict(symbol):
         data = json.load(json_file)
         print(data[symbol])
 
+def get_industry_group(symbol):
+    with open('sp500.json') as json_file:
+        dataList = json.load(json_file)
+        for dict_item in dataList:
+          for key in dict_item:
+            print(key)
+            #print(dict_item[key])
+
 if __name__ == "__main__":
     get_symbol_dict("fb")
     get_symbol_dict("amzn")
+    get_industry_group("xle")
