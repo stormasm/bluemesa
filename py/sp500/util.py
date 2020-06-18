cc65f51cb4a85b7338c0f73e5c1ec802cf93c04e
Michael Angerman <stormasm@gmail.com>
2020-06-18 16:43:16 -0700

    mw

--- py/sp500/util.py
+++ py/sp500/util.py
@@ -23,9 +23,12 @@ def get_all_symbols_sp500():
         dataList = json.load(json_file)
         for dict_item in dataList:
             for key in dict_item:
-                print(dict_item[key])
-                print("")
-
+                #print(dict_item[key])
+                #print("")
+                for item in dict_item[key]:
+                    print(item)
+                    symbols.add(item)
+    return(symbols)
 
 # get all of the symbols in a particular industy group
 def get_industry_group(symbol):
@@ -44,4 +47,5 @@ if __name__ == "__main__":
     print(ig)
     symbols = get_industry_group_symbols()
     print(symbols)
-    get_all_symbols_sp500()
+    symbols = get_all_symbols_sp500()
+    print(len(symbols))
