b58aa665a1b337389cbf8dd69398dd6712fdff94
Michael Angerman <stormasm@gmail.com>
2020-06-20 10:02:19 -0700

    adding assertions now to tests

--- py/tests/sp500util.py
+++ py/tests/sp500util.py
@@ -1,6 +1,11 @@
 from sp500 import util
 
 fb = util.get_symbol_name("fb")
-print(fb)
+#print(fb)
+name = "Facebook Inc A"
+assert(fb == name)
 amzn = util.get_symbol_name("amzn")
-print(amzn)
+#print(amzn)
+name = "Amazon.com Inc"
+assert(amzn == name)
+print("sp500util test completed")
