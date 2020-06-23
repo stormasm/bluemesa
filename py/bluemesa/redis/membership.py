d19d9682db44e356563f964ebeb2fcfea40786f7
Michael Angerman <stormasm@gmail.com>
2020-06-23 11:37:16 -0700

    mw

--- py/bluemesa/redis/membership.py
+++ py/bluemesa/redis/membership.py
@@ -2,7 +2,24 @@ import os
 import symboltable
 import util
 
-if __name__ == "__main__":
+def sdy_sp500():
+    set1 = util.redis_set_to_python_set("symbol-set-sdy")
+    set2 = util.redis_set_to_python_set("symbol-set-sp500")
+
+    intersection = set1.intersection(set2)
+    print("\nThese symbols are in the sp500")
+    print(intersection)
+    print(len(intersection))
+
+    difference = set1.difference(intersection)
+    print("\nThese symbols are not in the sp500")
+    print(difference)
+    print(len(difference))
+
+    print("\nThe total number of symbols in both sets")
+    print(len(set1))
+
+def aristocrats_sdy():
     sdy_set = util.redis_set_to_python_set("symbol-set-sdy")
     sp500_set = util.redis_set_to_python_set("symbol-set-sp500")
 
@@ -18,3 +35,7 @@ if __name__ == "__main__":
 
     print("\nThe total number of symbols in both sets")
     print(len(sdy_set))
+
+if __name__ == "__main__":
+    sdy_sp500()
+    #aristocrats_sdy()
