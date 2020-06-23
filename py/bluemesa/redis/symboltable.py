adc82ebf766786021ef8e1a83dbb5a4b977aadec
Michael Angerman <stormasm@gmail.com>
2020-06-22 18:08:44 -0700

    rename symboltable to symbol-hash

--- py/bluemesa/redis/symboltable.py
+++ py/bluemesa/redis/symboltable.py
@@ -17,17 +17,17 @@ def write_symbol_to_hash(rediskey,symbol,name):
     rc.hset(rediskey,symbol,name)
 
 def get_symbol_name(symbol):
-    name = rc.hget("symboltable",symbol)
+    name = rc.hget("symbol-hash",symbol)
     name = name.decode("utf-8")
     return(name)
 
 if __name__ == "__main__":
     name = util.get_symbol_name("fb")
     print(name)
-    write_symbol_to_hash("symboltable","fb",name)
+    write_symbol_to_hash("symbol-hash","fb",name)
     sp500 = util.get_all_symbols_sp500()
     for symbol in sp500:
         print(symbol)
         name = util.get_symbol_name(symbol)
-        write_symbol_to_hash("symboltable",symbol,name)
+        write_symbol_to_hash("symbol-hash",symbol,name)
         write_symbol_to_set("symbol-set-sp500",symbol)
