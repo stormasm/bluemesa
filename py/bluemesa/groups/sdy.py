e094f3977f07afa3344e946cbb5f0bfc8a33c0de
Michael Angerman <stormasm@gmail.com>
2020-06-23 15:28:09 -0700

    mw

--- py/bluemesa/groups/sdy.py
+++ py/bluemesa/groups/sdy.py
@@ -4,6 +4,7 @@ import pandas as pd
 path_bmtop = os.environ['BMTOP']
 sdy_csv_file = "/equity-data/groups/holdings-sdy.csv"
 
+# Read the company name and symbol from a csv file
 def sdy():
     filename = path_bmtop + sdy_csv_file
     df = pd.read_csv(filename, sep=',')
@@ -14,24 +15,10 @@ def sdy():
     # convert strings in array to lowercase
     tickers = map(str.lower, tickers)
     tickers = tuple(tickers)
-    print(tickers[5])
     names = map(str.lower,names)
     names = tuple(names)
-    print(names[5])
-
-
-def get_dict(filename):
-    df = pd.read_csv(filename, sep=',')
-    series = df['Symbol']
-    values = series.values
-    # convert strings in array to lowercase
-    vl = map(str.lower, values)
-    d = {}
-    symbol = get_symbol_from_filename(filename)
-    # convert the string array to a string tuple
-    st = tuple(vl)
-    d[symbol] = st
-    return(d)
+    for i, name in enumerate(tickers):
+        print(tickers[i],names[i])
 
 if __name__ == "__main__":
     sdy()
