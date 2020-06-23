0a4ad175866a2a45038dc103736da6364f267b4d
Michael Angerman <stormasm@gmail.com>
2020-06-22 18:28:03 -0700

    mw

--- py/bluemesa/redis/util.py
+++ py/bluemesa/redis/util.py
@@ -2,7 +2,7 @@ import redis
 
 r = redis.Redis(host='localhost', port=6379, db=0)
 
-def redis_set_to_set(key):
+def redis_set_to_python_set(key):
     members = set()
     rset = r.smembers(key)
     for value in rset:
@@ -14,5 +14,5 @@ if __name__ == "__main__":
     r.sadd('testset','red')
     r.sadd('testset','yellow')
     r.sadd('testset','green')
-    myset = redis_set_to_set('testset')
+    myset = redis_set_to_python_set('testset')
     print(myset)
