8e96d43a351284627d2643f4cb0af0ab8c87b813
Michael Angerman <stormasm@gmail.com>
2020-06-22 18:20:05 -0700

    mw

--- py/bluemesa/redis/membership.py
+++ py/bluemesa/redis/membership.py
@@ -4,14 +4,16 @@ from bluemesa.redis import symboltable
 if __name__ == "__main__":
     sdy_set = symboltable.get_symbols_from_set("symbol-set-sdy")
     sp500_set = symboltable.get_symbols_from_set("symbol-set-sp500")
+
     intersection = sdy_set.intersection(sp500_set)
-    print("These symbols are in the sp500")
+    print("\nThese symbols are in the sp500")
     print(intersection)
     print(len(intersection))
 
     difference = sdy_set.difference(intersection)
-    print("These symbols are not in the sp500")
+    print("\nThese symbols are not in the sp500")
     print(difference)
     print(len(difference))
 
+    print("\nThe total number of symbols in both sets")
     print(len(sdy_set))
