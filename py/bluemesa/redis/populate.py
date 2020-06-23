6bf2739c1c1c5dde30cc7f4e3e194ed48ccd0f45
Michael Angerman <stormasm@gmail.com>
2020-06-23 12:13:26 -0700

    refactor populate

--- py/bluemesa/redis/populate.py
+++ py/bluemesa/redis/populate.py
@@ -3,14 +3,13 @@ from bluemesa.util import lineutil
 import symboltable
 import util
 
+path = os.environ['BMTOP']
 sdy_file = "/bluemesa/config/symbols/sdy.txt"
 sdy_key = "symbol-set-sdy"
 aristocrats_file = "/bluemesa/config/symbols/aristocrats.txt"
 aristocrats_key = "symbol-set-aristocrats"
 
-if __name__ == "__main__":
-    path = os.environ['BMTOP']
-
+def sdy():
     path_in  = path + sdy_file
     symbols = lineutil.get_lines_as_set(path_in)
     for symbol in symbols:
@@ -18,9 +17,15 @@ if __name__ == "__main__":
     myset = util.redis_set_to_python_set(sdy_key)
     print("\n",myset)
 
+def aristocrats():
     path_in  = path + aristocrats_file
     symbols = lineutil.get_lines_as_set(path_in)
     for symbol in symbols:
         symboltable.write_symbol_to_set(aristocrats_key,symbol)
     myset = util.redis_set_to_python_set(aristocrats_key)
     print("\n",myset)
+
+
+if __name__ == "__main__":
+    sdy()
+    aristocrats()
