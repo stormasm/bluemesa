1a4970ed996bab542692970f9113bcdaf2fa5a2f
Michael Angerman <stormasm@gmail.com>
2020-06-26 13:48:00 -0700

    initial commit of sp500fun.py

---
+++ py/bluemesa/groups/sp500/sp500fun.py
@@ -0,0 +1,40 @@
+import json
+import os
+import pandas as pd
+from pathlib import PurePath
+import re
+
+def get_symbol_from_filename(filename):
+    pp = PurePath(filename)
+    # get the filename in the path
+    p1 = pp.parts[-1]
+    # get everything before the dot
+    p2 = re.split("\.",p1)[0]
+    # get everything after holdings-
+    p3 = re.split("holdings-",p2)[1]
+    return(p3)
+
+def get_dict(filename):
+    df = pd.read_csv(filename, sep=',')
+    series = df['Symbol']
+    values = series.values
+    # convert strings in array to lowercase
+    vl = map(str.lower, values)
+    d = {}
+    symbol = get_symbol_from_filename(filename)
+    # convert the string array to a string tuple
+    st = tuple(vl)
+    d[symbol] = st
+    return(d)
+
+if __name__ == "__main__":
+    path = os.environ['BMTOP']
+    path = path + '/equity-data/groups/sp500/200617'
+    files = os.listdir(path)
+    arr = []
+    for file in files:
+        filename = os.path.join(path, file)
+        d = get_dict(filename)
+        arr.append(d)
+    myj = json.dumps(arr)
+    print(myj)
