c3805d9ac96df2cb2a8545121dc7aceaff360802
Michael Angerman <stormasm@gmail.com>
2020-06-26 08:21:02 -0700

    starting to do some redis testing

--- py/bluemesa/redis/util.py
+++ py/bluemesa/redis/util.py
@@ -2,6 +2,7 @@
 # anything to do with bluemesa but could be used generically
 # across any project that uses python and redis
 
+import unittest
 import redis
 
 rc = redis.Redis(host='localhost', port=6379, db=0)
@@ -13,3 +14,23 @@ def redis_set_to_python_set(key):
         value = value.decode("utf-8")
         members.add(value)
     return(members)
+
+def redis_set_write(key,member):
+    rc.sadd(key,member)
+
+def redis_set_read(key,member):
+    value = rc.sismember(key,member)
+    return(value)
+
+def redis_delete(key):
+    rc.delete(key)
+
+if __name__ == "__main__":
+    redis_set_write("mykey","or")
+    val = redis_set_read("mykey","or")
+    assert val == True
+    val = redis_set_read("mykey","nm")
+    assert val == False
+    redis_delete("mykey")
+    val = rc.exists("mykey")
+    assert val == False
