6a758afa0290322fbde94e92039ee21dbe17056e
Michael Angerman <stormasm@gmail.com>
2020-06-26 08:44:54 -0700

    getfun: added in a redis check

--- py/bluemesa/yahoo_fin/getfun.py
+++ py/bluemesa/yahoo_fin/getfun.py
@@ -2,6 +2,7 @@ import datetime
 import os
 from yahoo_fin.stock_info import get_stats
 from bluemesa.util import lineutil
+from bluemesa.redis import util
 
 def get_day():
     x = datetime.datetime.now()
@@ -13,20 +14,23 @@ def build_file_name(symbol):
     filename = f"{symbol}-fun-{day}.csv"
     return(filename)
 
-def process(symbols,path):
+def process(symbols,path,key):
     for symbol in symbols:
         print(symbol)
         filename = build_file_name(symbol)
-        data = get_stats(symbol)
-        out_file = path + filename
-        data.to_csv(out_file)
+        bool = util.redis_set_read(key,symbol)
+        if not bool:
+            data = get_stats(symbol)
+            out_file = path + filename
+            data.to_csv(out_file)
+            util.redis_set_write(key,symbol)
 
 if __name__ == "__main__":
     path = os.environ['BMTOP']
     path_in  = path + '/bluemesa/config/symbols/sp500.txt'
     path_out = path + '/bluemesa/tmp/fun/in/'
     symbols = lineutil.get_lines_as_set(path_in)
-    process(symbols,path_out)
+    process(symbols,path_out,"symbol-check")
 
 # The variable symbols is always a Python set which is nice
 # because then we will not get any duplication of data
