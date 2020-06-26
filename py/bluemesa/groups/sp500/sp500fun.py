ec6db71496c35efccf947bd51cecf7d1eb2d4c72
Michael Angerman <stormasm@gmail.com>
2020-06-26 14:40:08 -0700

    mw

--- py/bluemesa/groups/sp500/sp500fun.py
+++ py/bluemesa/groups/sp500/sp500fun.py
@@ -4,6 +4,17 @@ import pandas as pd
 from pathlib import PurePath
 import re
 
+
+def get_symbol_from_filename1(filename):
+    pp = PurePath(filename)
+    # get the filename in the path
+    p1 = pp.parts[-1]
+    # get everything before the dot
+    p2 = re.split("-",p1)[0]
+    return(p2)
+
+
+
 def get_symbol_from_filename(filename):
     pp = PurePath(filename)
     # get the filename in the path
@@ -15,11 +26,15 @@ def get_symbol_from_filename(filename):
     return(p3)
 
 def get_dict1(filename):
-    print(filename)
+    #print(filename)
     df = pd.read_csv(filename, sep=',')
     series = df['Value']
     values = series.values
-    print(values)
+    d = {}
+    symbol = get_symbol_from_filename1(filename)
+    print(symbol)
+    d[symbol] = values
+    return(d)
 
 def get_dict(filename):
     df = pd.read_csv(filename, sep=',')
@@ -28,7 +43,8 @@ def get_dict(filename):
     # convert strings in array to lowercase
     vl = map(str.lower, values)
     d = {}
-    symbol = get_symbol_from_filename(filename)
+    symbol = get_symbol_from_filename1(filename)
+    print(symbol)
     # convert the string array to a string tuple
     st = tuple(vl)
     d[symbol] = st
@@ -42,6 +58,6 @@ if __name__ == "__main__":
     for file in files:
         filename = os.path.join(path, file)
         d = get_dict1(filename)
-        #arr.append(d)
+        arr.append(d)
     #myj = json.dumps(arr)
     #print(myj)
