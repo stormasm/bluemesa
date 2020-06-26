6bc2776f14ed996434926b95ec56eae3628f130d
Michael Angerman <stormasm@gmail.com>
2020-06-26 08:32:29 -0700

    more testing in redis

--- py/bluemesa/redis/util.py
+++ py/bluemesa/redis/util.py
@@ -2,7 +2,6 @@
 # anything to do with bluemesa but could be used generically
 # across any project that uses python and redis
 
-import unittest
 import redis
 
 rc = redis.Redis(host='localhost', port=6379, db=0)
@@ -29,6 +28,16 @@ if __name__ == "__main__":
     k = "mykey"
     redis_set_write(k,"or")
     redis_set_write(k,"ca")
+    redis_set_write(k,"tx")
+    val = redis_set_to_python_set(k)
+    ok1 = "or" in val
+    ok2 = "ca" in val
+    ok3 = "tx" in val
+    ok4 = "nm" in val
+    assert ok1 == True
+    assert ok2 == True
+    assert ok3 == True
+    assert ok4 == False
     val = redis_set_read(k,"or")
     assert val == True
     val = redis_set_read(k,"nm")
