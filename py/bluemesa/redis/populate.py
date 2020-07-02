41252cad566620f2544255d5c5f179aec1382874
Michael Angerman <stormasm@gmail.com>
2020-07-01 20:54:11 -0700

    iwv up and running across the board

--- py/bluemesa/redis/populate.py
+++ py/bluemesa/redis/populate.py
@@ -8,6 +8,8 @@ sdy_file = "/bluemesa/config/symbols/sdy.txt"
 sdy_key = "symbol-set-sdy"
 aristocrats_file = "/bluemesa/config/symbols/aristocrats.txt"
 aristocrats_key = "symbol-set-aristocrats"
+iwv_file = "/bluemesa/config/symbols/iwv.txt"
+iwv_key = "symbol-set-iwv"
 
 def sdy():
     path_in  = path + sdy_file
@@ -25,7 +27,15 @@ def aristocrats():
     myset = util.redis_set_to_python_set(aristocrats_key)
     print("\n",myset)
 
+def iwv():
+    path_in  = path + iwv_file
+    symbols = lineutil.get_lines_as_set(path_in)
+    for symbol in symbols:
+        symboltable.write_symbol_to_set(iwv_key,symbol)
+    myset = util.redis_set_to_python_set(iwv_key)
+    print("\n",myset)
 
 if __name__ == "__main__":
     sdy()
     aristocrats()
+    iwv()
