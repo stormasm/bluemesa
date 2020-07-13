93512eed2394c95f97ce494085537d59a2a823b2
Michael Angerman <stormasm@gmail.com>
2020-07-13 12:36:29 -0700

    starting to work on taking all of the csv files and combining them into one csv file which will then be converted to sql

---
+++ py/bluemesa/groups/sp500/sp500fun_to_csv.py
@@ -0,0 +1,49 @@
+import json
+import os
+import pandas as pd
+from pathlib import PurePath
+import re
+
+def modify_array_values(input):
+    arr = []
+    for value in input:
+        mytype = type(value)
+        bool = isinstance(value,(float,tuple))
+        if not bool:
+            arr.append(value)
+        else:
+            arr.append("N/A")
+    return(arr)
+
+def get_symbol_from_filename(filename):
+    pp = PurePath(filename)
+    # get the filename in the path
+    p1 = pp.parts[-1]
+    # get everything before the dot
+    p2 = re.split("-fun",p1)[0]
+    return(p2)
+
+def get_dict(filename):
+    df = pd.read_csv(filename, sep=',')
+    series = df['Value']
+    values = series.values
+    values = modify_array_values(values)
+    d = {}
+    symbol = get_symbol_from_filename(filename)
+    st = tuple(values)
+    d[symbol] = st
+    return(d)
+
+# Remember to remove the Readme in the directory
+# py sp500fun.py > sp500fun.json
+if __name__ == "__main__":
+    path = os.environ['BMTOP']
+    path = path + '/equity-fun/sp500/data/20-07-12'
+    files = os.listdir(path)
+    arr = []
+    for file in files:
+        filename = os.path.join(path, file)
+        d = get_dict(filename)
+        arr.append(d)
+    myj = json.dumps(arr)
+    print(myj)
