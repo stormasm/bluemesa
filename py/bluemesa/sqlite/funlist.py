377a20e7fe85513349e9eba0494f2c790eb1158e
Michael Angerman <stormasm@gmail.com>
2020-07-13 09:36:00 -0700

    mw

--- py/bluemesa/sqlite/funlist.py
+++ py/bluemesa/sqlite/funlist.py
@@ -21,10 +21,10 @@ def getfiles(mypath):
             files.add(os.path.join(mypath, file))
     return(files)
 
-def write_data_to_redis_list(symbol,index,data):
+def write_data_to_sqlite(symbol,index,data):
     print(symbol,index,data)
 
-def write_file_to_redis(filename):
+def write_file_to_sqlite(filename):
     symbol = get_symbol_from_filename(filename)
     rediskey = symbol + "-fun"
     print(symbol)
@@ -34,11 +34,11 @@ def write_file_to_redis(filename):
         # do not read the first line of the csv file
         next(funreader)
         for row in funreader:
-            write_data_to_redis_list(rediskey,row[0],row[2])
+            write_data_to_sqlite(rediskey,row[0],row[2])
 
 if __name__ == "__main__":
     path = os.environ['BMTOP']
     path = path + '/equity-fun/sp500/data/20-07-12'
     files = getfiles(path)
     for file in files:
-        write_file_to_redis(file)
+        write_file_to_sqlite(file)
