4f16076a7fa059bee5093defc1c112a740a224dd
Michael Angerman <stormasm@gmail.com>
2020-07-13 08:48:17 -0700

    change input data directory to data/20-07-12

--- py/bluemesa/redis/funlist.py
+++ py/bluemesa/redis/funlist.py
@@ -38,7 +38,7 @@ def write_file_to_redis(filename):
 
 if __name__ == "__main__":
     path = os.environ['BMTOP']
-    path = path + '/equity-fun/sp500/20-06-26'
+    path = path + '/equity-fun/sp500/data/20-07-12'
     files = getfiles(path)
     for file in files:
         write_file_to_redis(file)
