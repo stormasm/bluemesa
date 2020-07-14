3bc730a21a199b71ec2ab3e0f5c4c5f5e5ea2d47
Michael Angerman <stormasm@gmail.com>
2020-07-14 09:22:28 -0700

    mw

--- py/bluemesa/sqlite/cashflow.py
+++ py/bluemesa/sqlite/cashflow.py
@@ -5,14 +5,16 @@ def init(filename):
     conn = sqlite3.connect(filename)
     return conn
 
-def getCashFlow(filename):
-    csq = init(filename)
-    cursor = csq.cursor()
-    t = ('ibm',)
-    cursor.execute('SELECT * FROM sp500fun WHERE symbol=?', t)
-    print(cursor.fetchone())
+def getCashFlow(filename, symbol):
+    conn = init(filename)
+    cursor = conn.cursor()
+    t = (symbol,)
+    cursor.execute('SELECT "operating-cash-flow", "levered-free-cash-flow" FROM sp500fun WHERE symbol=?', t)
+    resultset = cursor.fetchone()
+    return(resultset)
 
 if __name__ == "__main__":
     path = os.environ['BMTOP']
     path = path + '/bluemesa/data/sp500fun.db'
-    getCashFlow(path)
+    resultset = getCashFlow(path,'ibm')
+    print(resultset)
