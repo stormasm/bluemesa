460babe8346483d7ead972cc334a1c810828c669
Michael Angerman <stormasm@gmail.com>
2020-07-30 16:16:31 -0700

    starting up the pv3x series with ui data as the test case

---
+++ py/bluemesa/edgar/pv3xui.py
@@ -0,0 +1,21 @@
+import os
+from bluemesa.edgar.pv3x import XBRLParser, GAAPSerializer, DEISerializer
+
+def parse(file):
+    print("\nData for ",file)
+    xbrl_parser = XBRLParser()
+    xbrl = xbrl_parser.parse(open(file))
+    #gaap_obj = xbrl_parser.parseGAAP(xbrl)
+    gaap_obj = xbrl_parser.parseGAAP(xbrl, doc_date="20200331", context="current", ignore_errors=0)
+    serializer = GAAPSerializer()
+    result = serializer.dump(gaap_obj)
+    print(result)
+
+if __name__ == "__main__":
+
+    f1 = 'ubnt-20200331_htm.xml'
+
+    path = os.environ['BMTOP']
+    path = path + '/equity-data/edgar/'
+    path = path + f1
+    parse(path)
