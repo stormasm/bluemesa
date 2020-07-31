991c47a63115da2d8e97d34bef93a379e404f52b
Michael Angerman <stormasm@gmail.com>
2020-07-30 17:30:03 -0700

    add in a 2nd test case file for ui data from 2019 12/31

--- py/bluemesa/edgar/pv3xui.py
+++ py/bluemesa/edgar/pv3xui.py
@@ -5,7 +5,8 @@ def parse(file):
     print("\nData for ",file)
     xbrl_parser = XBRLParser()
     xbrl = xbrl_parser.parse(open(file))
-    gaap_obj = xbrl_parser.parseGAAP(xbrl, doc_date="20200331", context="current", ignore_errors=0)
+#   gaap_obj = xbrl_parser.parseGAAP(xbrl, doc_date="20200331", context="current", ignore_errors=0)
+    gaap_obj = xbrl_parser.parseGAAP(xbrl, doc_date="20191231", context="current", ignore_errors=0)
     serializer = GAAPSerializer()
     result = serializer.dump(gaap_obj)
     print(result)
@@ -13,8 +14,9 @@ def parse(file):
 if __name__ == "__main__":
 
     f1 = 'ubnt-20200331_htm.xml'
+    f2 = 'ubnt-20191231_htm.xml'
 
     path = os.environ['BMTOP']
     path = path + '/equity-data/edgar/'
-    path = path + f1
+    path = path + f2
     parse(path)
