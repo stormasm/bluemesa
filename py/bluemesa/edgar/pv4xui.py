07d056263b666182bfbfcddb9e1bc403a60638e7
Michael Angerman <stormasm@gmail.com>
2020-07-31 18:26:58 -0700

    mw

--- py/bluemesa/edgar/pv4xui.py
+++ py/bluemesa/edgar/pv4xui.py
@@ -6,7 +6,12 @@ def parse(file):
     xbrl_parser = XBRLParser()
     xbrl = xbrl_parser.parse(open(file))
 #   gaap_obj = xbrl_parser.parseGAAP(xbrl, doc_date="20200331", context="current", ignore_errors=0)
-    gaap_obj = xbrl_parser.parseGAAP(xbrl, doc_date="20191231", context="current", ignore_errors=0)
+#   gaap_obj = xbrl_parser.parseGAAP(xbrl, doc_date="20191231", context="current", ignore_errors=0)
+
+#   I want this to work so I do not need to know the date for every filing...
+#   A blank doc_date should get the current date...
+    gaap_obj = xbrl_parser.parseGAAP(xbrl, context="current", ignore_errors=0)
+
     serializer = GAAPSerializer()
     result = serializer.dump(gaap_obj)
     print(result)
