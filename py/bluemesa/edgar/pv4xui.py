7aef2cc35fb18feeef138373ba12049ee9b1cd80
Michael Angerman <stormasm@gmail.com>
2020-11-08 19:55:33 -0800

    getting all files from a directory that ends in .xml and then pulling the date from the filename and passing it into the parse function

--- py/bluemesa/edgar/pv4xui.py
+++ py/bluemesa/edgar/pv4xui.py
@@ -1,27 +1,33 @@
 import os
 from bluemesa.edgar.pv4x import XBRLParser, GAAPSerializer
 
-def parse(file):
+def parse(file,doc_date):
     print("\nData for ",file)
     xbrl_parser = XBRLParser()
     xbrl = xbrl_parser.parse(open(file))
-#   gaap_obj = xbrl_parser.parseGAAP(xbrl, doc_date="20200331", context="current", ignore_errors=0)
-#   gaap_obj = xbrl_parser.parseGAAP(xbrl, doc_date="20191231", context="current", ignore_errors=0)
 
-    gaap_obj = xbrl_parser.parseGAAP(xbrl, doc_date="20200930")
+    gaap_obj = xbrl_parser.parseGAAP(xbrl, doc_date)
 
     serializer = GAAPSerializer()
     result = serializer.dump(gaap_obj)
     print(result)
 
+def get_doc_date(filename):
+    date = filename.split("-")[2::][0]
+    date = date.split("_")[0]
+    return(date)
+
+def getfiles(mypath):
+    files = set()
+    for file in os.listdir(mypath):
+        if file.endswith(".xml"):
+            files.add(os.path.join(mypath, file))
+    return(files)
+
 if __name__ == "__main__":
-
-    f1 = 'ubnt-20200930_htm.xml'
-    f2 = 'ubnt-20200630_htm.xml'
-    f3 = 'ubnt-20200331_htm.xml'
-    f4 = 'ubnt-20191231_htm.xml'
-
     path = os.environ['BMTOP']
     path = path + '/edgar-data/ubnt/'
-    path = path + f1
-    parse(path)
+    filenames = getfiles(path)
+    for filename in filenames:
+        doc_date = get_doc_date(filename)
+        parse(filename,doc_date)
